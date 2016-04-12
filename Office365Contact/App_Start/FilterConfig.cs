f3b533f283eeb88d25f6f8ee1bddb9456e0efe15
sandipmalaviya07 <sandipmalaviya07@gmail.com>
2016-04-12 11:27:23 +0530

    Initial Commit

---
+++ Office365Contact/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Office365Contact
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Office365Contact/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Office365Contact.Startup))]
+namespace Office365Contact
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
